9068755245bcf81e50dfb9f05858da2f55c083a1
Damaso Magno <limamdamaso@gmail.com>
2021-02-26 16:58:53 -0300

    secure atualizado

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.post("/feedback",(req,res)=>{
     const transporter = nodemailer.createTransport({
         host:process.env.NODEMAILER_HOST,
         port:process.env.NODEMAILER_PORT,
-        secure:process.env.NODEMAILER_SECURE,
+        secure:process.env.NODEMAILER_SECURE === "true",
         auth: {
             user:process.env.NODEMAILER_USER,
             pass:process.env.NODEMAILER_PASS
